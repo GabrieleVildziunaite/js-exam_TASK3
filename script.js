d2a3f2650753835b3ede73cdb28d73839c62f9b7
GabrieleVildziunaite <54136608+GabrieleVildziunaite@users.noreply.github.com>
2022-10-26 20:20:36 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,56 @@
+/* ------------------------------ TASK 3 -----------------------------------
+Parašykite JS kodą, kuris leis vartotojui paspaudus ant mygtuko "Show users"
+pamatyti vartotojus iš Github API (endpoint'as pateiktas žemiau).
+
+Paspaudus mygtuką "Show users":
+1. Pateikiamas informacijos atvaizdavimas <div id="output"></div> bloke
+1.1. Informacija, kuri pateikiama: "login" ir "avatar_url" reikšmės (kortelėje)
+2. Žinutė "Press "Show Users" button to see users" turi išnykti;
+"
+Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
+-------------------------------------------------------------------------- */
+
+const ENDPOINT = "https://api.github.com/users";
+
+async function getData(url) {
+	try {
+		const response = await fetch(url);
+		const responseData = await response.json();
+		return responseData;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
+}
+
+async function getDataFromUrl(url, func) {
+	const data = await getData(url);
+	func(data);
+	console.log(data);
+}
+
+function populateUsers(data) {
+	const output = document.getElementById("output");
+    
+	data.forEach((dataItem) => {
+        const outputbox = document.createElement("div");
+
+        const login = document.createElement("div");
+        login.textContent = dataItem.login;
+        login.classList.add("login");
+
+        const avatarPicture = document.createElement("img");
+        avatarPicture.src = dataItem.avatar_url;
+        avatarPicture.classList.add("avatar_pic");
+
+        outputbox.append(login, avatarPicture)
+        output.append(outputbox);
+	});
+}
+
+
+
+getDataFromUrl(ENDPOINT, populateUsers);
+
+
+
